4d32a95c943d6897f009d85fd1ba0cf1148207ed
Georgina <georginamircheska@gmail.com>
2025-07-18 11:11:51 +0200

    Feat: implementation selectors

---
+++ app.js
@@ -0,0 +1,39 @@
+//SELECTORS FOR CALCULATING DURATION :
+// inputs type range
+const loanDuration = document.getElementById("loan-duration");
+const interestDuration = document.getElementById("interest-dutarion");
+const paymentDuration = document.getElementById("payment-duration");
+// span - user input
+const currentLoanDuration = document.getElementById("current-loan-duration");
+const currentInterestDuration = document.getElementById(
+  "current-interest-dutarion"
+);
+const currentPaymentDuration = document.getElementById(
+  "current-payment-duration"
+);
+// input type text - result
+const inputResultDuration = document.getElementById("input-result-duration");
+// form - button
+const inputDuration = document.getElementById("input-duration-form");
+
+//SELECTORS FOR CALCULATING PAYMENT QTY :
+// inputs type range
+const loanPayment = document.getElementById("loan-payment");
+const interestPayment = document.getElementById("interest-payment");
+const duartionPayment = document.getElementById("duration-payment");
+// span -  user input
+const currentLoanPayment = document.getElementById("current-loan-payment");
+const currentInterestPayment = document.getElementById(
+  "current-interest-payment"
+);
+const currentDurationPayment = document.getElementById(
+  "current-duration-payment"
+);
+// input type text - result
+const inputResultPayment = document.getElementById("input-result-payment");
+// form - button
+const inputPaymentForm = document.getElementById("input-payment-form");
+
+// LISTENERS FOR DURATION :
+
+// LISTENERS FOR PAYMENT QTY :
